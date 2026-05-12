ffcfa55ee749294b279ae9b56f062a7cef722367
Dayah Madani <dayah@madani.com>
2026-05-12 12:28:58 +0700

    Fix database deployment - force add sqlite + runtime migration

--- api/index.php
+++ api/index.php
@@ -1,15 +1,26 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
 $dbPath = '/tmp/database.sqlite';
 
 if (!file_exists($dbPath)) {
-    $source = __DIR__ . '/../database/database.sqlite';
-    if (file_exists($source)) {
-        copy($source, $dbPath);
-    }
+    touch($dbPath);
 }
 
 $_ENV['DB_DATABASE'] = $dbPath;
 putenv("DB_DATABASE=$dbPath");
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->call('migrate', ['--force' => true]);
+$kernel->call('db:seed', ['--force' => true]);
+
+$app->handleRequest(Request::capture());
